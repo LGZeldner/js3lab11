321c2ddc86703bc461e1355225539002721a1771
LGZeldner <alicelidd@yandex.ru>
2019-10-18 14:43:42 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {isNullOrUndefined} from 'util';
+import {Sensor} from './shared/models/sensor.module';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'js3lab11';
+  sensors: Sensor[] = [];
+  newSensorName: string;
+  newSensorStatus: string;
+  sensorsCount: number;
+  constructor() {
+    this.sensorsCount = 10;
+    for (let i = 0; i < this.sensorsCount; i++) {
+      this.sensors.push(new Sensor (i, `Датчик_${i}`));
+    }
+    console.log(this.sensors);
+  }
+  on_delete_sensor(index: number) {
+    this.sensors.splice(index, 1); /* удалили из массива элемент */
+  }
+  on_add_sensor() {
+    this.sensors.push(new Sensor (++this.sensorsCount, this.newSensorName, (this.newSensorStatus === 'true')));
+  }
 }
